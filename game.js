4b8764fa80e878b20b86ccbe5351cf1687be6060
Adivya Jain <95540661+adivya-jain@users.noreply.github.com>
2023-03-04 01:10:14 +0530

    Add files via upload

--- game.js
+++ game.js
@@ -2,27 +2,46 @@
 
 var buttonColours = ["red", "blue", "green", "yellow"]
 var gamePattern = []
+var userClickedPattern=[]
+
+$(".btn").click(function handler()
+{
+    var userChosenColour=$(this).attr("id");
+
+    userClickedPattern.push(userChosenColour);
+
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+}
+)
+
+
 
 
 function nextSequence() {
-    var randomNumber = Math.random();
-    randomNumber = randomNumber * 4;
-    randomNumber = Math.floor(randomNumber)
+    var randomNumber = Math.floor(Math.random()*4);
+
 
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour)
 
     $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-    var audio = new Audio("sounds/"+ randomChosenColour +".mp3");
-    audio.play();
-
-    $(".btn").click(function handler()
-    {
-        var userChosenColour=
-    }
-    
-    )
-
+   playSound(randomChosenColour);
 }
 
 
+function playSound(name)
+{
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour)
+{
+    $("#"+currentColour).addClass("pressed")
+
+    setTimeout(function(){
+        $("#" + currentColour).removeClass("pressed");
+    },100)
+
+}
